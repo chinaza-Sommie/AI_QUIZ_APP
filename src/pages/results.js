43d77f435ff16ab55f8fe49dd2adc171a2920306
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2026-02-26 13:52:40 +0000

    added routes

--- src/pages/results.js
+++ src/pages/results.js
@@ -2,18 +2,75 @@
 
 function Results() {
   return (
-     <div className='h-screen flex justify-center items-center'>
+     <div className='flex justify-center items-center py-10'>
       <div className="w-[45%]">
 
         {/* top section */}
-        <div className="bg-[#151923] text-center border border-[#202531] py-15 rounded-xl">
-            <div className="mb-10 border-b-4 border-[]">
+        <div className="bg-[#151923] text-center border border-[#202531] py-12 rounded-xl">
+            <div className="inline-block mb-10 border-b-4 border-[#7056f6] pb-3">
                 <h2 className="text-white text-5xl font-bold">100%</h2>
-                <p className="text-[#7589a3] text-lg pt-1"> 10/10 correct</p>
+                <p className="text-[#7589a3] text-base pt-1"> 10/10 correct</p>
             </div>
             <div>
                 <h2 className="text-white text-2xl font-semibold"> You're a Legend</h2>
-                <p className="text-[#7589a3] text-lg pt-1">HTML Quiz</p>
+                <p className="text-[#7589a3] text-base pt-1">HTML Quiz</p>
+            </div>
+        </div>
+
+        {/* question review */}
+        <div className="bg-[#151923] border border-[#202531] p-10 mt-5 rounded-xl">
+            <h3 className="text-white text-2xl font-bold mb-5"> Review Answers</h3>
+            
+            <div>
+                <div className="bg-[#281a25] border border-[#67272f] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base "> <span className="text-[#ef4343]">Your Anwser: var</span> <span className="text-white">||</span> <span className="text-[#81cba1]">Correct Answer: let</span></p>
+                </div>
+
+                <div className="bg-[#152226] border border-[#18593b] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base text-[#93ecb8]"> Your Answer: let </p>
+                </div>
+
+                <div className="bg-[#281a25] border border-[#67272f] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base "> <span className="text-[#ef4343]">Your Anwser: var</span> <span className="text-white">||</span> <span className="text-[#81cba1]">Correct Answer: let</span></p>
+                </div>
+
+                <div className="bg-[#281a25] border border-[#67272f] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base "> <span className="text-[#ef4343]">Your Anwser: var</span> <span className="text-white">||</span> <span className="text-[#81cba1]">Correct Answer: let</span></p>
+                </div>
+
+                <div className="bg-[#281a25] border border-[#67272f] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base "> <span className="text-[#ef4343]">Your Anwser: var</span> <span className="text-white">||</span> <span className="text-[#81cba1]">Correct Answer: let</span></p>
+                </div>
+
+                <div className="bg-[#281a25] border border-[#67272f] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base "> <span className="text-[#ef4343]">Your Anwser: var</span> <span className="text-white">||</span> <span className="text-[#81cba1]">Correct Answer: let</span></p>
+                </div>
+
+                <div className="bg-[#281a25] border border-[#67272f] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base "> <span className="text-[#ef4343]">Your Anwser: var</span> <span className="text-white">||</span> <span className="text-[#81cba1]">Correct Answer: let</span></p>
+                </div>
+
+                <div className="bg-[#281a25] border border-[#67272f] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base "> <span className="text-[#ef4343]">Your Anwser: var</span> <span className="text-white">||</span> <span className="text-[#81cba1]">Correct Answer: let</span></p>
+                </div>
+
+                <div className="bg-[#281a25] border border-[#67272f] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base "> <span className="text-[#ef4343]">Your Anwser: var</span> <span className="text-white">||</span> <span className="text-[#81cba1]">Correct Answer: let</span></p>
+                </div>
+
+                <div className="bg-[#281a25] border border-[#67272f] px-5 py-3 mb-5 rounded-xl">
+                    <h4 className="text-white text-lg font-semibold"> Question</h4>
+                    <p className="text-base "> <span className="text-[#ef4343]">Your Anwser: var</span> <span className="text-white">||</span> <span className="text-[#81cba1]">Correct Answer: let</span></p>
+                </div>
             </div>
         </div>
       </div>
