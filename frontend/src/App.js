c44aad994859becbc504d4346dbc3f935b0061b8
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2026-04-03 11:12:48 +0100

    created frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import SelectStack from './pages/selectStack';
+import Questions from './pages/questions';
+import Layout from './pages/layout';
+import Results from './pages/results';
+import { useState, useEffect } from 'react';
+// import '';
+
+function App() {
+  const [selected, setSelected] = useState("");
+  const [answers, setAnswers] = useState({});
+
+  return (
+    // <div className="min-h-screen bg-grid">
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home/>} />
+          <Route path="/select" element={<SelectStack setSelected={setSelected} selected={selected} />} />
+          <Route path="/select/:selected" element={<Questions answers={answers} setAnswers={setAnswers} />} />
+          <Route path="/results" element={<Results answers={answers} setAnswers={setAnswers}/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,19 @@
+
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <div className="bg-[#151923] border border-[#7589a3] w-[30%] text-center py-9 rounded rounded-md shadow-md shadow-[#7589a3]">
+        <div className="text-[#7056f6] font-bold text-3xl mb-3"> {'</>'}  </div>
+        <h1 className="text-white font-bold text-3xl mb-3">DevQuiz</h1>
+        <p className="text-[#7589a3] text-lg mb-8"> Test Your Web Dev Knowledge </p>
+        <Link to={"/select"}>
+            <button className="bg-[#7056f6] text-base text-white py-2 px-6 rounded rounded-md tansition delay-150 duration-300 ease-in-out hover:cursor-pointer hover:bg-[#5f47d8] "> Get Started </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
