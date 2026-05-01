bf526561777e269ed91c22036a64c13278980c8e
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2026-05-01 20:32:22 +0100

    made alteration to home page

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -7,7 +7,7 @@ function Home() {
       <div className="bg-[#151923] border border-[#7589a3] w-[30%] text-center py-9 rounded rounded-md shadow-md shadow-[#7589a3]">
         <div className="text-[#7056f6] font-bold text-3xl mb-3"> {'</>'}  </div>
         <h1 className="text-white font-bold text-3xl mb-3">DevQuiz</h1>
-        <p className="text-[#7589a3] text-lg mb-8"> Test Your Web Dev Knowledge </p>
+        <p className="text-[#7589a3] text-lg mb-8"> Test Your Web Dev Knowledge s</p>
         <Link to={"/select"}>
             <button className="bg-[#7056f6] text-base text-white py-2 px-6 rounded rounded-md tansition delay-150 duration-300 ease-in-out hover:cursor-pointer hover:bg-[#5f47d8] "> Get Started </button>
         </Link>
